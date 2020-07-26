b815dc9872ea8dce37757bb36e61db4f72a4d380
Kobi <kobi_tk@interia.pl>
2020-07-27 00:20:57 +0200

    Add prettier rules and husky hook

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,35 +3,35 @@
  */
 
 (function () {
-  documentReady(init);
+    documentReady(init);
 })();
 
 function init() {
-  const bgImageElem = document.getElementById('bgImg');
+    const bgImageElem = document.getElementById('bgImg');
 
-  bgImageElem.addEventListener('load', function () {
-    const bodyElem = document.body;
-    bodyElem.style.backgroundImage = `url('${this.src}')`;
-    bodyElem.style.opacity = 1;
+    bgImageElem.addEventListener('load', function () {
+        const bodyElem = document.body;
+        bodyElem.style.backgroundImage = `url('${this.src}')`;
+        bodyElem.style.opacity = 1;
 
-    const loaderElem = document.getElementById('loader');
-    loaderElem.style.opacity = 0;
-    loaderElem.addEventListener('transitionend', function () {
-      this.style.display = 'none';
-      const menuElem = document.getElementById('menu-icon');
-      menuElem.style.visibility = 'visible';
-      menuElem.style.opacity = 1;
+        const loaderElem = document.getElementById('loader');
+        loaderElem.style.opacity = 0;
+        loaderElem.addEventListener('transitionend', function () {
+            this.style.display = 'none';
+            const menuElem = document.getElementById('menu-icon');
+            menuElem.style.visibility = 'visible';
+            menuElem.style.opacity = 1;
+        });
     });
-  });
 }
 
 function documentReady(func) {
-  if (
-    document.readyState === 'complete' ||
-    document.readyState === 'interactive'
-  ) {
-    func.call();
-  } else {
-    document.addEventListener('DOMContentLoaded', func);
-  }
+    if (
+        document.readyState === 'complete' ||
+        document.readyState === 'interactive'
+    ) {
+        func.call();
+    } else {
+        document.addEventListener('DOMContentLoaded', func);
+    }
 }
